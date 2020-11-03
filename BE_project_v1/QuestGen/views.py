27fe949ccf28ca877e2fb8b20c694d9ceda44d53
Parth Panchal <51913848+HtraP1001@users.noreply.github.com>
2020-11-03 14:16:20 +0530

    login / signup basic template done

--- BE_project_v1/QuestGen/views.py
+++ BE_project_v1/QuestGen/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'QuestGen/index.html')
 
+def login(request):
+    return render(request, 'QuestGen/login.html')
 
-# Create your views here.
+def taketest(request):
+    return render(request, 'QuestGen/taketest.html')
