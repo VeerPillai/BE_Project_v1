bb2a773691f1368627280ffb75c24c6076c17938
Parth Panchal <51913848+HtraP1001@users.noreply.github.com>
2020-12-04 10:01:35 +0530

    test

--- BE_project_v1/QuestGen/urls.py
+++ BE_project_v1/QuestGen/urls.py
@@ -5,6 +5,7 @@ from django.urls import include
 urlpatterns = [
     path('', views.index, name='index'),
     path('taketest/', views.taketest, name='taketest'),
+    path('quiz/', views.quiz, name='quiz'),
     
     path('account/', include('account.urls')),
 ]
--- BE_project_v1/QuestGen/views.py
+++ BE_project_v1/QuestGen/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def index(request):
     return render(request, 'QuestGen/index.html')
 
+
 def taketest(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
     return render(request, 'QuestGen/taketest.html')
+
+def quiz(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
+    return render(request, 'QuestGen/quiz.html')
